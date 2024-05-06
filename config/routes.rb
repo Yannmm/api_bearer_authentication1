098a1f6cb03847ee3b7ccc1df1c882827e89bb0d
Yan Meng <yannmm@foxmail.com>
2024-05-06 22:17:43 +0800

    add api_token

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  namespace :api do 
+    namespace :v1 do 
+      defaults format: :json do 
+        get 'home/index', to: 'home#index'
+      end
+    end
+  end
 end
